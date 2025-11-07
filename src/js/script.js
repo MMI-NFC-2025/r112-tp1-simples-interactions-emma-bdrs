aac66c60230775ccb6274b13e4deea08efa7eb90
emma-bdrs <emma.badarous@edu.univ-fcomte.fr>
2025-11-07 08:52:24 +0100

    script java fini

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,13 @@ const AMettreEnRouge = document.querySelector("H2");
 AMettreEnRouge.style.color = "red";
 
 const button = document.querySelector("button");
-const EnRougeSuiteAClick = document.querySelector("button"); // const ajoutée ici
+const EnRougeSuiteAClick = document.querySelector("button"); 
 
 EnRougeSuiteAClick.addEventListener("click", (evt) => {
   EnRougeSuiteAClick.style.color = "red";
 });
 
-const collH2 = document.querySelectorAll("H2"); // const ajoutée ici
+const collH2 = document.querySelectorAll("H2"); 
 
 collH2.forEach((elm) => {
   elm.addEventListener("click", (evt) => {
