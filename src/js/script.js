c29c79b3c58ae53cfde53e899d59581b61229a8a
emma-bdrs <emma.badarous@edu.univ-fcomte.fr>
2025-11-07 08:51:20 +0100

    coder en java

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 // TODO
+const AMettreEnRouge = document.querySelector("H2");
+AMettreEnRouge.style.color = "red";
+
+const button = document.querySelector("button");
+const EnRougeSuiteAClick = document.querySelector("button"); // const ajoutée ici
+
+EnRougeSuiteAClick.addEventListener("click", (evt) => {
+  EnRougeSuiteAClick.style.color = "red";
+});
+
+const collH2 = document.querySelectorAll("H2"); // const ajoutée ici
+
+collH2.forEach((elm) => {
+  elm.addEventListener("click", (evt) => {
+    evt.target.style.color = "red";
+  });
+});
